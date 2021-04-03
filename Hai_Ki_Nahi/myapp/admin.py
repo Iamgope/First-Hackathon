39fe42d3da34c32248909ae5d4b713fb2eb4e30a
RAZI AHMAD <60400893+Razihmad@users.noreply.github.com>
2021-04-03 16:51:14 +0530

    userdetail extended

--- Hai_Ki_Nahi/myapp/admin.py
+++ Hai_Ki_Nahi/myapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item,UserDetail
 
 # Register your models here.
 
 admin.site.register(Item)
+admin.site.register(UserDetail)
+
 
--- Hai_Ki_Nahi/myapp/models.py
+++ Hai_Ki_Nahi/myapp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 class Item(models.Model):
     tags = [('choice1','Books'),('choice2','Instruments'),('choice3','Bikes'),('choice4','Clothes'),]
     username = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -11,3 +10,7 @@ class Item(models.Model):
 
     def __str__(self):
         return self.item_name
+
+class UserDetail(models.Model):
+    lended = models.ManyToManyField(to=Item)
+    phone =  models.PositiveIntegerField()
