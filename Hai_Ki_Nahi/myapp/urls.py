befe367d977b8f082f89d6c2bae82465dab4cc0d
Manish Kumar Gupt <57751918+mingul26@users.noreply.github.com>
2021-04-03 21:40:01 +0530

    linked categories page

--- Hai_Ki_Nahi/myapp/urls.py
+++ Hai_Ki_Nahi/myapp/urls.py
@@ -1,10 +1,13 @@
 from os import name
 from django.urls import path
 from myapp import views
+
+app_name = 'myapp'
+
 urlpatterns = [
-    path('',views.home,name = 'home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('upload/',views.upload,name= 'upload'),
-    path('categories/', views.category, name='category'),
-    path('item_des/',views.item_desc,name= 'description'),
+    path('',views.home, name = 'home'),
+    path('dashboard/', views.dashboard, name = 'dashboard'),
+    path('upload/',views.upload, name = 'upload'),
+    path('categories/', views.category, name = 'category'),
+    path('item_des/',views.item_desc, name = 'description'),
 ]
